752b5ae525c59bae065ab36f5063f984d5b8ae8a
steven <stevenmatthew.tanamas@my.jcu.edu.au>
2021-12-01 12:08:30 +0700

    Remove unnecessary lines from subject_reader.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -17,14 +17,9 @@ def get_data():
     input_file = open(FILENAME)
     file_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
         file_data.append(parts)
     input_file.close()
     return file_data
