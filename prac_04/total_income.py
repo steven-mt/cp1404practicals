71efaa5cd0284a4b9db46765ec37f8356f3d23d3
steven <stevenmatthew.tanamas@my.jcu.edu.au>
2021-12-01 10:27:20 +0700

    Refactor cumulative total income program to use print report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,6 +13,11 @@ def main():
         income = float(input("Enter income for month {}: ").format(str(month)))
         incomes.append(income)
 
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes, number_of_months):
+    """Print the income report."""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
