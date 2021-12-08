003488c6dd29bd431268c954d3db59ba861e4d78
steven <stevenmatthew.tanamas@my.jcu.edu.au>
2021-12-08 12:54:53 +0700

    Update to sort words

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,5 +5,8 @@ for word in words:
     if word not in word_to_length:
         word_to_length[word] = len(word)
 
-for key, value in word_to_length.items():
+word_to_length_list = [(key, value) for key, value in word_to_length.items()]
+word_to_length_list.sort()
+
+for key, value in word_to_length_list:
     print(f"{key} : {value}")
