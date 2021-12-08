748c69463f92bb12861b233748e7345ddbbf4085
steven <stevenmatthew.tanamas@my.jcu.edu.au>
2021-12-08 11:31:00 +0700

    Add loop to print dictionary neatly

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,12 +8,13 @@ File needs reformatting
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
+for key, value in CODE_TO_NAME.items():
+    print(f"{key:3} is {value}")
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
-   
