c89ded696e1708bdf4bcd73a13426cb89e14ffae
steven <stevenmatthew.tanamas@my.jcu.edu.au>
2021-12-08 13:04:28 +0700

    Update to align output

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,12 +1,15 @@
 string_input = input("Text: ").lower()
 word_to_length = {}
 words = string_input.split()
+maximum_length = 0
 for word in words:
-    if word not in word_to_length:
+    if word not in word_to_length: # check if word is in dictionary
         word_to_length[word] = len(word)
+        if len(word) > maximum_length:  # get the length of the longest word
+            maximum_length = len(word)
 
 word_to_length_list = [(key, value) for key, value in word_to_length.items()]
 word_to_length_list.sort()
 
 for key, value in word_to_length_list:
-    print(f"{key} : {value}")
+    print(f"{key:{maximum_length}} : {value}")
