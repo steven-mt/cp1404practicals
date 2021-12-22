d69565cf569d2922c8bbcf01f49052c5944c345e
steven <stevenmatthew.tanamas@my.jcu.edu.au>
2021-12-22 10:58:22 +0700

    Add languages.py

---
+++ prac_06/languages.py
@@ -0,0 +1,3 @@
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
