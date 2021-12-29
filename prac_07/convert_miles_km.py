23942491278c24f7393df70cf1c1b3ff904d32ed
steven <stevenmatthew.tanamas@my.jcu.edu.au>
2021-12-29 13:42:42 +0700

    Add basic conversion and increment functions

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,12 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+MILES_IN_KM = 1.60934
+
 
 class MilesToKilometresApp(App):
+
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_convert(self, value):
+        """Handle conversion of miles to km."""
+        result = float(value) * MILES_IN_KM
+        self.root.ids.output_label.text = str(result)
+
+    def handle_increment(self, miles, change):
+        """Handle increment or decrement of input and updates output."""
+        result = float(miles) + change
+        self.root.ids.input_number.text = str(result)
+        self.handle_convert(self.root.ids.input_number.text)
 
 
 MilesToKilometresApp().run()
