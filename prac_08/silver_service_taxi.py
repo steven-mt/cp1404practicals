745541f9c1cdcc44a34b2ca55ace4d159ecdc96c
steven <stevenmatthew.tanamas@my.jcu.edu.au>
2022-01-05 11:52:20 +0700

    Add silver_service_taxi.py and silver_service_taxi_test.py

---
+++ prac_08/silver_service_taxi.py
@@ -0,0 +1,16 @@
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
